3392b8e3129d1e4d930d15315c018a1e49e5c1c7
ronald-0699 <60554928+ronald-0699@users.noreply.github.com>
2021-07-28 20:35:39 -0600

    Update tiposReflectores.c

--- Analizador_Lexico/tiposReflectores.c
+++ Analizador_Lexico/tiposReflectores.c
@@ -5,7 +5,8 @@
 
 void  verificar (char c);
 // variables globales
-char tiposReflectores[]={"1","2","3","4"};
+int limRef=0;
+char tiposReflectores[limRef]={ };
 
 int main()
 {
@@ -14,3 +15,14 @@ int main()
     return 0;
 }
 // funcion para verificar  el tipo de reflectores
+void verificar()
+{
+    char tiposReflectores[4]={"1","2","3","4"};
+    char tipoSeleccionado ="";
+    for(int i=0; i<limRef;i++){
+        if(limfRef=="1"){
+                tipoSeleccionado=limRef;
+        };
+    };
+
+}
