bedf11b09b474823aa0fb0972f3e0af328b1fec5
DidyanN98 <53923372+DidyanN98@users.noreply.github.com>
2021-07-28 18:53:03 -0600

    Update ColorReflector.c

--- Analizador_Lexico/ColorReflector.c
+++ Analizador_Lexico/ColorReflector.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<string.h>
-
+//definicion del tamaño del arreglo
 #define clr 4
 
 int main(){
@@ -9,7 +9,8 @@ int main(){
 	
 	return 0;
 }
- //funcion para seleccionar el color del reflector
+
+//funcion de seleccion del color de reflectores
 void ClrReflector(){
 	char *colores[clr]={
 	"amarillo",
@@ -19,21 +20,51 @@ void ClrReflector(){
 	};
 	
 	char *temp;
-	int i, j;
+	int i, j, id;
+	
 
 	
-// muestra de los colores 
+
 	for( i=0; i<clr-1; i++ )
 		for( j=i+1; j<clr; j++ )
 			if ( strcmp(colores[i], colores[j]) > 0 ) {
 				temp = colores[i];
 				colores[i] = colores[j];
 				colores[j] = temp;
-			}
-	//impresion
+		}
+	
+	//impresion de la lista de colores con id
 
 	printf( "Lista de Colores disponibles en los Reflectores:\n" );
 	for( i=0; i<clr; i++ )
-		printf( "  %s.\n", colores[i] );
+		printf( "%i. %s\n ",i+1,colores[i] );
 	
+	
+	//seleccion de colores
+		
+		printf( "seleccionar el id numerico del color \n" );
+		printf("\nId: ");
+		scanf("%i",&id);
+		
+		
+		switch(id){
+	    	case 1:
+	    	        printf("\n Color seleccionado es el amarillo "); 
+	    		break;
+	    	case 2:
+	    		    printf("\n Color seleccionado es el azul");
+			    break;
+			case 3:			    
+	    	        printf("\n Color seleccionado es el rojo");
+			    break;
+			case 4:			    
+	    	        printf("\n Color seleccionado es el verde");
+			    break;		
+			default:
+				printf("\nLa opcion que escogio es invalida "); 
+				
+		}
+	   
+		
+
 }
