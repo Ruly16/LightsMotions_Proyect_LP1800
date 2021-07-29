828c57323f4f31544c50458379d2f95cc3541909
DidyanN98 <53923372+DidyanN98@users.noreply.github.com>
2021-07-28 18:05:34 -0600

    Update TamañoReflector.c

--- Analizador_Lexico/TamañoReflector.c
+++ Analizador_Lexico/TamañoReflector.c
@@ -1,6 +1,31 @@
 #include<stdio.h>
 
-int main(){
 
-return 0;
+
+int main(){
+	
+	TMatriz();   
+
+
+  return 0;
+}
+
+//Función del tamaño del reflector
+int TMatriz(){
+	
+  //tamaños disponibles
+	 int tamano[] = {1,2,4};
+    int i, opc;
+    
+	
+	printf("Escoger dimension de reflector ");
+	//lector del array
+	   for(i=0; i<3; i++){
+		printf("%i, " , tamano[i]);
+	    } 
+	printf("\nOpcion: ");    
+	    scanf("%f",&opc);
+  
+  //condicional
+	   
 }
