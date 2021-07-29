a4f81a7fd132d9b8ab8f6146b8eb91cca6a4a487
ronald-0699 <60554928+ronald-0699@users.noreply.github.com>
2021-07-28 22:31:36 -0600

    Update identificadorFuente.c

--- Analizador_Lexico/identificadorFuente.c
+++ Analizador_Lexico/identificadorFuente.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 // identificador de fuente de agua
+// Prototipo de funciones
+void identificarFuente(char a[]);
+// declaracion de variables globales.
+char numFuentes[]={"1","2","3","4"};
+
 
 int main()
 {
 
     return 0;
 }
+void identificarFuente(char b[])
+{
+
+};
