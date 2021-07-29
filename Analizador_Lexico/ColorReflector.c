b22efd9d5844856e682aa0ba1f8cac05a061ad85
DidyanN98 <53923372+DidyanN98@users.noreply.github.com>
2021-07-28 18:29:35 -0600

    Update and rename ColorReflector to ColorReflector.c

---
+++ Analizador_Lexico/ColorReflector.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<string.h>
+
+#define clr 4
+
+int main(){
+	
+	ClrReflector();
+	
+	return 0;
+}
+
+void ClrReflector(){
+	char *colores[clr]={
+	"amarillo",
+	"azul",
+	"rojo",
+	"verde"
+	};
+	
+	char *temp;
+	int i, j;
+
+	
+
+	for( i=0; i<clr-1; i++ )
+		for( j=i+1; j<clr; j++ )
+			if ( strcmp(colores[i], colores[j]) > 0 ) {
+				temp = colores[i];
+				colores[i] = colores[j];
+				colores[j] = temp;
+			}
+
+	printf( "Lista de Colores disponibles en los Reflectores:\n" );
+	for( i=0; i<clr; i++ )
+		printf( "  %s.\n", colores[i] );
+	
+}
