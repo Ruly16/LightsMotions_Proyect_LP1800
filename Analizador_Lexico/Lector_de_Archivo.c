dba001a0e7ff913fe3083e312e16faa92cb99372
RulyFunez <rulyhn@hotmail.com>
2021-08-02 15:55:59 -0600

    Lector de archivos

--- Analizador_Lexico/Lector_de_Archivo.c
+++ Analizador_Lexico/Lector_de_Archivo.c
@@ -15,6 +15,29 @@ int main(){
 
 void lector(){
 
-	}
+	/*Función  que lee de un archivo*/
+   printf("\n Bienvenidos al Lector\n");
+
+    FILE* archivoLectura=NULL;
+    char* nombrearchivo= "Programa.txt";
+    char leer[100];
+
+    archivoLectura = fopen(nombrearchivo, "r");
+    if(archivoLectura==NULL) 
+	return -1;
+
+    fscanf(archivoLectura," %[^\n]",&leer);
+    printf("Lectura: %s\n",leer);
+    fscanf(archivoLectura," %[^\n]",&leer);
+    printf("Lectura: %s\n",leer);
+    fscanf(archivoLectura," %[^\n]",&leer);
+    printf("Lectura: %s\n",leer);
+    
+    fclose(archivoLectura);
+
+    return 0;
+}
+
+	
 	
 
