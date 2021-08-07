47c371313cdaeb5d3dfef2fb98cdbcc0ffcce62d
RulyFunez <rulyhn@hotmail.com>
2021-08-06 23:49:53 -0600

    Tipos de Reflector

--- Analizador_Lexico/tiposReflectores.c
+++ Analizador_Lexico/tiposReflectores.c
@@ -1,30 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 // prototipos de funciones
 
-void  verificar (char c[]);
+bool  verificar ();
 // variables globales
-int limRef=0;
-char tiposReflectores[limRef]={ };
+int limRef=4;
+int tiposReflectores[ ]={1,2,3,4 };
 
 int main()
 {
-    char b[]={""};
-    verificar(b[]);
+    
+   int b [ ] = {1, 2, 3, 4};
+   verificar();
     return 0;
 }
 // funcion para verificar  el tipo de reflectores
-void verificar(
+bool verificar( int a [])
 {
-    char tiposReflectores[4]={"1","2","3","4"};e
-    char tipoSeleccionado ="";
-    for(int i=0; i<limRef;i++){
-        if(limfRef=="1"){
-                tipoSeleccionado=limRef;
-                printf("ek tipo de reflector seleccionado es:",tipoSeleccionado);
+   int opcion;
+    int tipoSeleccionado;
+    int i;
+    
+      printf("\n---------- Tipos de Reflectores---------- ");
+    printf("\n 1. Un solo color ");
+    printf("\n 2. Dos colores ");
+    printf("\n 3. Cuatro Colores ");
+    
+    printf("\nIngrese el tipo de reflector: ");
+     scanf("%i", &opcion);
+     
+    	switch(opcion){
+	    	case 1:
+	    	        printf("\n Reflector seleccionado es de Un Solo color "); 
+	    		break;
+	    	case 2:
+	    		    printf("\n Reflector seleccionado es de Dos Colores");
+			    break;
+			case 3:			    
+	    	        printf("\n Refletor seleccionado es de Cuatro Colores");
+			    break;					
+			default:
+				printf("\nLa opcion que escogio es invalida "); 
+				
+		} 
+ 
+
+
         };
 
-    };
 
-}
+
+
